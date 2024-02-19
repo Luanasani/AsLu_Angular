38e03ee41ae93827d9b0e55de71ecfa3fd6e5373
Luanasani <luanasani48@gmail.com>
2024-02-19 15:58:29 +0100

    Routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { SharedModule } from './shared/shared.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SharedModule],
+  imports: [RouterOutlet, SharedModule, CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ExampleAComponent } from './example-a/example-a.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'example-aComponent', component: ExampleAComponent}
+];
